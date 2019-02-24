800bfa6c9ac35c2e1a9d1247ca3b293a3a92793d
maclanzallamas <maclanzallamas@hotmail.com>
2019-02-24 12:19:22 +0000

    First commit changes

--- src/resumeData.js
+++ src/resumeData.js
@@ -1,53 +1,48 @@
 let resumeData = {
     "imagebaseurl":"https://rbhatia46.github.io/",
-    "name": "Rahul Bhatia",
-    "role": "Frontend Developer and Data Scientist",
-    "linkedinId":"Your LinkedIn Id",
+    "name": "Mario Llamas Lanza",
+    "role": "Frontend Developer",
+    "linkedinId":"www.linkedin.com/in/mariollamaslanza",
     "skypeid": "Your skypeid",
     "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
     "socialLinks":[
         {
           "name":"linkedin",
-          "url":"https://www.linkedin.com/in/rahul-bhatia-67ba08121/",
+          "url":"www.linkedin.com/in/mariollamaslanza",
           "className":"fa fa-linkedin"
         },
         {
           "name":"github",
-          "url":"http://github.com/rbhatia46",
+          "url":"https://github.com/maclanzallamas",
           "className":"fa fa-github"
-        },
-        {
-          "name":"skype",
-          "url":"http://twitter.com/rbhatia46",
-          "className":"fa fa-twitter"
         }
       ],
     "aboutme":"I am currently a pre-final year student at The LNM Institute of Information Technology and pursuing my B.Tech from here. I am a self taught Full Stack Web Developer, currently diving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
-    "address":"India",
+    "address":"Glasgow, Scotland",
     "website":"https://rbhatia46.github.io",
     "education":[
       {
-        "UniversityName":"The LNM Insitute of Information Technology",
-        "specialization":"Some specialization",
-        "MonthOfPassing":"Aug",
-        "YearOfPassing":"2020",
-        "Achievements":"Some Achievements"
+        "UniversityName":"Masters in Data Science",
+        "specialization":"Data Science",
+        "MonthOfPassing":"Sept",
+        "YearOfPassing":"2018",
+        "Achievements":"Masters with Honours"
       },
       {
-        "UniversityName":"Some University",
-        "specialization":"Some specialization",
-        "MonthOfPassing":"Jan",
-        "YearOfPassing":"2018",
-        "Achievements":"Some Achievements"
+        "UniversityName":"University of Leon",
+        "specialization":"Computer Science Engineering",
+        "MonthOfPassing":"Jun",
+        "YearOfPassing":"2016",
+        "Achievements":"Average of 7,66 over 10"
       }
     ],
     "work":[
       {
-        "CompanyName":"Some Company",
-        "specialization":"Some specialization",
+        "CompanyName":"Scottish Power",
+        "specialization":"Front End development",
         "MonthOfLeaving":"Jan",
         "YearOfLeaving":"2018",
-        "Achievements":"Some Achievements"
+        "Achievements":"React"
       },
       {
         "CompanyName":"Some Company",

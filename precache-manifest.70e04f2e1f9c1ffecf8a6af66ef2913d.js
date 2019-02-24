2a41e14144b106ee5a59b228cc01a5693a927da1
maclanzallamas <maclanzallamas@hotmail.com>
2019-02-24 18:17:27 +0000

    Updates

---
+++ precache-manifest.70e04f2e1f9c1ffecf8a6af66ef2913d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4ef5e139691095579935",
+    "url": "/static/js/main.4ef5e139.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "4ef5e139691095579935",
+    "url": "/static/css/main.2d30f555.chunk.css"
+  },
+  {
+    "revision": "ce6df89908f10a438271cceba7f73ae8",
+    "url": "/index.html"
+  }
+];
